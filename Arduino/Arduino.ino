b8105a9208cb43b7aa0bf506b92589b8f5cfcb93
wurrch <fb8ee747-fa1d-48a8-a637-7fb1da4802dc@aleeas.com>
2022-11-11 15:54:40 +0000

    Update Arduino.ino

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -7,22 +7,22 @@ void setup() {
 
 void loop() {
   // 'Primary Button','Secondary Button','Joystick Button','Joystick X','Joystick Y','Wheel Angle','Clutch','Break','Accelerator'
-  Serial.print(digitalRead(2));
+  Serial.print(digitalRead(2)); // Primary Button
   Serial.print(",");
-  Serial.print(digitalRead(3));
+  Serial.print(digitalRead(3)); // Secondary Button
   Serial.print(",");
-  Serial.print(digitalRead(4));
+  Serial.print(digitalRead(4)); // Joystick Button
   Serial.print(",");
-  Serial.print(analogRead(A4));
+  Serial.print(analogRead(A4)); // Joystick X
   Serial.print(",");
-  Serial.print(analogRead(A5));
+  Serial.print(analogRead(A5)); // Joystick Y
   Serial.print(",");
-  Serial.print(analogRead(A0));
+  Serial.print(analogRead(A0)); // Wheel Angle
   Serial.print(",");
-  Serial.print(analogRead(A1));
+  Serial.print(analogRead(A1)); // Clutch Angle
   Serial.print(",");
-  Serial.print(analogRead(A2));
+  Serial.print(analogRead(A2)); // Break Angle
   Serial.print(",");
-  Serial.println(analogRead(A3));
+  Serial.println(analogRead(A3)); // Accelerator Angle
   delay(20);
 }
