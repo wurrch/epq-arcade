0c1395479d2875c6f8d6a78477aaa0e6cf8917a5
wurrch <fb8ee747-fa1d-48a8-a637-7fb1da4802dc@aleeas.com>
2022-11-11 17:55:20 +0000

    Update Arduino.ino

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,28 +1,28 @@
 void setup() {
   Serial.begin(9600);      // Initialize the serial port and set the baud rate to 9600
-  pinMode(2, INPUT_PULLUP); // Primary Button 
-  pinMode(3, INPUT_PULLUP); // Secondary Button
-  pinMode(4, INPUT_PULLUP); // Joystick Button
+  pinMode(10, INPUT_PULLUP); // Primary Button 
+  pinMode(9, INPUT_PULLUP); // Secondary Button
+  pinMode(8, INPUT_PULLUP); // Joystick Button
 }
 
 void loop() {
-  // 'Primary Button','Secondary Button','Joystick Button','Joystick X','Joystick Y','Wheel Angle','Clutch','Break','Accelerator'
-  Serial.print(digitalRead(2)); // Primary Button
+  // 'Primary Button','Secondary Button','Joystick X','Joystick Y','Joystick Button','Clutch','Break','Accelerator','Wheel Angle'
+  Serial.print(digitalRead(10)); // Primary Button
   Serial.print(",");
-  Serial.print(digitalRead(3)); // Secondary Button
+  Serial.print(digitalRead(9)); // Secondary Button
   Serial.print(",");
-  Serial.print(digitalRead(4)); // Joystick Button
+  Serial.print(analogRead(A0)); // Joystick X
   Serial.print(",");
-  Serial.print(analogRead(A4)); // Joystick X
+  Serial.print(analogRead(A1)); // Joystick Y
   Serial.print(",");
-  Serial.print(analogRead(A5)); // Joystick Y
+  Serial.print(digitalRead(8)); // Joystick Button
   Serial.print(",");
-  Serial.print(analogRead(A0)); // Wheel Angle
+  Serial.print(analogRead(A2)); // Clutch Angle
   Serial.print(",");
-  Serial.print(analogRead(A1)); // Clutch Angle
+  Serial.print(analogRead(A3)); // Break Angle
   Serial.print(",");
-  Serial.print(analogRead(A2)); // Break Angle
+  Serial.print(analogRead(A4)); // Accelerator Angle
   Serial.print(",");
-  Serial.println(analogRead(A3)); // Accelerator Angle
+  Serial.println(analogRead(A5)); // Wheel Angle
   delay(20);
 }
